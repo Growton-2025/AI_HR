f5e13e0d22c586f45c2a70ff4817d7197120fefc
Nethranand <nethranand@Nethranands-MacBook-Air.local>
2026-06-26 12:29:06 +0530

    Remove temporary debug scripts

--- test_latency.sh
+++
@@ -1,4 +0,0 @@
-for i in {1..5}
-do
-  curl -w "Time: %{time_total}s\n" -s "http://127.0.0.1:8000/api/outreach/chat/linkedin/0/13096" | tail -n 1
-done
--- test_loop.sh
+++
@@ -1,8 +0,0 @@
-> /tmp/ai_hr_backend.log
-for i in {1..3}
-do
-  echo "Curl $i:"
-  curl -w "Time: %{time_total}s\n" -s "http://127.0.0.1:8000/api/outreach/chat/linkedin/0/13096?force=true" | tail -n 1
-done
-echo "Logs:"
-grep "DEBUG TIMING" /tmp/ai_hr_backend.log
